917528ca27ea7fbdb7cd52e951e1e3712862cd2e
srvo-scripts <strummermchone7@gmail.com>
2026-05-24 00:12:08 -0700

    Update bot activities to include new messages

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Bypassing BattlEye",
+        name: "Bypassing RICOCHET",
+        name: "Delivering Products Instantly",
         // Activity type number (0 = Playing).
         type: 0, 
       },
